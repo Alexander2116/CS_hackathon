27fcc04c0a1cb700abb36c8123866f11b9b276f0
ksenijakovalenka24 <ksenija.kovalenka@postgrad.manchester.ac.uk>
2024-04-13 16:36:46 +0100

    Update animation.py

--- animation.py
+++ animation.py
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     # To view the animation, run the script with the command 'manim -pql custom_animation.py CustomAnimation'
     Example().render()
 
-    #hi!  
+    #hi! hi
