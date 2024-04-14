f90add8a49a0418f58d64287c754e5658ed0a36d
Aleksander Kedziora <aleksander2116k@gmail.com>
2024-04-14 08:31:03 +0100

    main_win

--- GUI/main_win.py
+++ GUI/main_win.py
@@ -6,13 +6,141 @@
 
 import sys
 from PyQt5.QtWidgets import * #QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QDesktopWidget, QFormLayout, QLineEdit, QMessageBox, QListWidget, QMainWindow, QAction
-from PyQt5.QtCore import Qt, QMimeData, QPoint
+from PyQt5.QtCore import Qt, QMimeData, QPoint, pyqtSignal
 from Drag_Drop import DD_Window
 from animated_back import VideoBackground
 from PyQt5.QtGui import QIcon, QDrag
 
-class MainWindow(QMainWindow):
+
+class enter_parametrs(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Enter parametrs")
+        self.setGeometry(100, 100, 400, 200)
+        layout = QFormLayout(self)
+        
+        self.p = QLineEdit(self)
+        self.vel = QLineEdit(self)
+        self.m = QLineEdit(self)
+        self.s = QLineEdit(self)
+        
+        layout.addRow("Position (x,y)", self.p)
+        layout.addRow("Velocity (x,y)", self.vel)
+        layout.addRow("Mass", self.m)
+        layout.addRow("Size (H,W)", self.s)
+        
+        self.ok_button = QPushButton("OK", self)
+        self.ok_button.clicked.connect(self.ok_button_pressed)
+        layout.addRow(self.ok_button)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        
+    def ok_button_pressed(self):
+        if(self.p.text() == "" or self.vel.text() == "" or self.m.text() == "" or self.s.text() == ""):
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Please fill all the fields")
+            msg.setWindowTitle("Error")
+            msg.setWindowFlags(Qt.WindowStaysOnTopHint)
+            msg.exec_()
+            
+        elif(len(self.p.text().split(",")) != 3 or len(self.vel.text().split(",")) != 3 or len(self.s.text().split(",")) != 2):
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Incorrect values, follow the pattern")
+            msg.setWindowTitle("Error")
+            msg.setWindowFlags(Qt.WindowStaysOnTopHint)
+            msg.exec_()
+            
+        elif(not ((self.p.text().split(",")[0].isnumeric() and self.p.text().split(",")[1].isnumeric() and self.p.text().split(",")[2].isnumeric()) and (self.vel.text().split(",")[0].isnumeric() and self.vel.text().split(",")[1].isnumeric() and self.vel.text().split(",")[2].isnumeric()) and self.m.text().isnumeric() and \
+             (self.s.text().split(",")[0].isnumeric() and self.s.text().split(",")[1].isnumeric()))):
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Incorrect values, enter numeric values only")
+            msg.setWindowTitle("Error")
+            msg.setWindowFlags(Qt.WindowStaysOnTopHint)
+            msg.exec_()
+            
+        else:
+            self.accept()
+        
+    def get_params(self):
+        return {
+            "pos":(float(self.p.text().split(",")[0]), float(self.p.text().split(",")[1]), float(self.p.text().split(",")[2])),
+            "vel":(float(self.vel.text().split(",")[0]), float(self.vel.text().split(",")[1]), float(self.vel.text().split(",")[2])),
+            "mass":float(self.m.text()),
+            "size":(float(self.s.text().split(",")[0]), float(self.s.text().split(",")[1] ))
+        }
+
+class PopUp(QDialog):
+    textEntered = pyqtSignal(str)
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Pop Up")
+        self.setGeometry(100, 100, 400, 200)
+        layout = QVBoxLayout(self)
+        label = QLabel("This is a pop up window")
+        layout.addWidget(label)
+        button1 = QPushButton("Rock",self)
+        button2 = QPushButton("Satellite",self)
+        button1.clicked.connect(self.button1_pressed)
+        button2.clicked.connect(self.button2_pressed)
+        
+        layout.addWidget(button1)
+        layout.addWidget(button2)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        
+        self.show()
+
+    def button1_pressed(self):
+        
+        self.textEntered.emit("rock")
+        self.accept()
     
+    def button2_pressed(self):
+        self.textEntered.emit("satellite")
+        self.accept()
+    
+    
+class DragLabel(QLabel):
+    def __init__(self, title, parent):
+        super().__init__(title, parent)
+        self.setAcceptDrops(True)
+        self.setFrameShape(QFrame.Box)
+        self.setAlignment(Qt.AlignCenter)
+        self.setStyleSheet("border: 2px dashed #aaa;")
+        self.setFixedSize(200, 200)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasImage or event.mimeData().hasText():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasImage:
+            self.setPixmap(event.mimeData().image)
+        elif event.mimeData().hasText():
+            self.setText(event.mimeData().text())
+        event.accept()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.drag_start_position = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if not (event.buttons() & Qt.LeftButton):
+            return
+
+        if (event.pos() - self.drag_start_position).manhattanLength() < QApplication.startDragDistance():
+            return
+class MainWindow(QMainWindow):
+    object_params = []
     params = {
         "name":"space rocks!"
     }
@@ -29,8 +157,8 @@ class MainWindow(QMainWindow):
         self.setWindowTitle(self.params["name"])
         self.setGeometry(0, 0, screen_rect.width(), screen_rect.height())
         # Create actions for the toolbar
-        exitAct = QAction('Objects', self)
-        exitAct.triggered.connect(self.DD_win)
+        exitAct = QAction('Add new', self)
+        exitAct.triggered.connect(self.add_menu)
 
         # Create the toolbar
         toolbar = self.addToolBar('Toolbar')
@@ -44,13 +172,6 @@ class MainWindow(QMainWindow):
         self.scene = QGraphicsScene()
         self.graphics_view.setScene(self.scene)
 
-        # Add a rectangle item to the scene
-        self.rect_item = QGraphicsRectItem(0, 0, 100, 100)
-        self.rect_item.setBrush(Qt.blue)
-        self.scene.addItem(self.rect_item)
-
-        # Enable item movement
-        self.rect_item.setFlag(QGraphicsRectItem.ItemIsMovable)
         """
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
@@ -74,6 +195,37 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_widget)"""
 
         
+    def add_menu(self):
+        dialog = PopUp()
+        dialog.textEntered.connect(self.on_text_entered)
+        dialog.exec_()
+        
+        if(self.add_new_object == "rock" or self.add_new_object == "satellite"):
+            param_win = enter_parametrs()
+            if param_win.exec_() == QDialog.Accepted:
+                param = param_win.get_params()
+            print(param)
+            param_win.exec_()
+            size = param["size"]
+            pos = param["pos"]
+            temp = QGraphicsRectItem(pos[0], pos[1], size[0], size[1])
+            temp.setFlag(QGraphicsRectItem.ItemIsMovable)
+            
+            if(self.add_new_object == "rock"):
+                temp.setBrush(Qt.red)
+            elif(self.add_new_object == "satellite"):
+                temp.setBrush(Qt.blue)
+                
+            self.scene.addItem(temp)
+        
+    def run_animation(self):
+        for item in self.scene.items():
+            if isinstance(item, QGraphicsRectItem):
+                print(f"Found QGraphicsRectItem at position ({item.pos().x()}, {item.pos().y()})")
+    
+    def on_text_entered(self, text):
+        self.add_new_object = text
+        
     def DD_win(self):
         self.DD_window = DD_Window()
         
@@ -92,6 +244,13 @@ class MainWindow(QMainWindow):
         else:
             event.ignore()
         
+    def get_object_from_scene(self):
+        # Iterate over all items in the scene
+        for item in self.scene.items():
+            # Check if the item is a QGraphicsRectItem
+            if isinstance(item, QGraphicsRectItem):
+                # Here you can access the QGraphicsRectItem and perform actions
+                print(f"Found QGraphicsRectItem at position ({item.pos().x()}, {item.pos().y()})")
         
 
 if __name__ == '__main__':
